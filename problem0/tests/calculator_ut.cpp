919029b27c28c7aa6955527867b76a6f2b95f6b5
Philipp Jung <jung@volumegraphics.com>
2020-07-23 13:54:54 +0200

    WIP Restructure and transition to doctest

--- problem0/tests/calculator_ut.cpp
+++ problem0/tests/calculator_ut.cpp
@@ -1,6 +1,4 @@
-#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
-#include <catch2/catch.hpp>
-
+#include "doctest.h"
 #include <calculator.h>
 
 #include "generate.h"
--- tests/main.cpp
+++
@@ -1,6 +0,0 @@
-#include <iostream>
-
-int main()
-{
-    std::cout << "test_main" << std::endl;
-}
