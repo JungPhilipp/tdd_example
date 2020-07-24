e7b8d17b40cd6be79099db9935285c3d1331e94f
Philipp Jung <jung@volumegraphics.com>
2020-07-24 09:34:18 +0200

    Reverse string: Add comments

--- problem0/src/problem0.cpp
+++ problem0/src/problem0.cpp
@@ -2,7 +2,6 @@
 #include <print.h>
 
 void problem0::reverse(char *str) {
-  // use std:: swap
   auto index = 0;// limited length (consider using long/size_t)
   while(str[index] != '\0'){
     index++;
@@ -10,7 +9,7 @@ void problem0::reverse(char *str) {
   auto lhs = 0;
   auto rhs = index - 1;
 
-  using std::swap;
+  using std::swap; // ADL for swap specialization
   while(lhs < rhs) {
     swap(str[lhs++], str[rhs--]);
   }
