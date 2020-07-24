804e94871d60610bf66ee48e7bad7491bf6cac33
Philipp Jung <jung@volumegraphics.com>
2020-07-24 09:36:59 +0200

    Reverse string: Add special test cases

--- problem0/src/problem0.cpp
+++ problem0/src/problem0.cpp
@@ -7,7 +7,7 @@ void problem0::reverse(char *str) {
     index++;
   }
   auto lhs = 0;
-  auto rhs = index - 1;
+  auto rhs = index - 1; // since rhs is signed this won't be a problem
 
   using std::swap; // ADL for swap specialization
   while(lhs < rhs) {
--- problem0/tests/problem0_ut.cpp
+++ problem0/tests/problem0_ut.cpp
@@ -4,9 +4,29 @@
 using namespace problem0;
 
 TEST_CASE("Reverse String"){
-  SUBCASE("Reverse non-empty string"){
+  SUBCASE("Reverse non-empty odd-length string"){
     char str[] = "test test";
     problem0::reverse(str);
     CHECK(std::string("tset tset") == str);
+  }
+  SUBCASE("Reverse non-empty even-length string"){
+    char str[] = "testtest";
+    problem0::reverse(str);
+        CHECK(std::string("tsettset") == str);
+  }
+  SUBCASE("Reverse one character string"){
+    char str[] = "t";
+    problem0::reverse(str);
+        CHECK(std::string("t") == str);
+  }
+  SUBCASE("Reverse two character string"){
+    char str[] = "ab";
+    problem0::reverse(str);
+        CHECK(std::string("ba") == str);
+  }
+  SUBCASE("Reverse empty string"){
+    char str[] = "";
+    problem0::reverse(str);
+        CHECK(std::string("") == str);
   }
 }
