6ccf51c990053f78e0754afdde92ca0ed478ae3e
Philipp Jung <philipp.jung@mailbox.org>
2020-07-31 11:08:53 +0200

    Fix smart_pointer example to handle self-assignment correctly

--- problem0/include/smart_ptr.h
+++ problem0/include/smart_ptr.h
@@ -1,48 +1,58 @@
 #pragma once
 
-#include <utility>
 #include "print.h"
+#include <utility>
 
 namespace problem0 {
 
-  template<class T>
-  class smart_ptr {
-  public:
-    // Compromise to avoid impl. a make_smart_ptr function
-    // Call by value and move into storage
-    explicit smart_ptr(T* value) : ref_count(new int(1)), value(value) {}
-    smart_ptr(smart_ptr<T> &rhs): value(rhs.value), ref_count(rhs.ref_count){
-      (*ref_count)++;
-    }
+template<class T>
+class smart_ptr
+{
+public:
+  // Compromise to avoid impl. a make_smart_ptr function
+  // Call by value and move into storage
+  explicit smart_ptr(T* value)
+    : ref_count(new int(1))
+    , value(value)
+  {}
+  smart_ptr(smart_ptr<T>& rhs)
+    : value(rhs.value)
+    , ref_count(rhs.ref_count)
+  {
+    (*ref_count)++;
+  }
 
-    auto operator=(smart_ptr<T> const& rhs) -> smart_ptr<T>& {
-      (*ref_count)--;
-      if(*ref_count == 0){
-        delete ref_count;
-        delete value;
-      }
-      this->ref_count = rhs.ref_count;
-      this->value = rhs.value;
-      (*ref_count)++;
+  auto operator=(smart_ptr<T> const& rhs) -> smart_ptr<T>&
+  {
+    if (this == rhs)
       return *this;
+    
+    (*ref_count)--;
+    if (*ref_count == 0) {
+      delete ref_count;
+      delete value;
     }
+    this->ref_count = rhs.ref_count;
+    this->value = rhs.value;
+    (*ref_count)++;
+    return *this;
+  }
 
-    auto operator*() -> T { return *value; }
+  auto operator*() -> T { return *value; }
 
-    auto operator->() -> T * { return value; }
+  auto operator->() -> T* { return value; }
 
-
-
-    ~smart_ptr(){
-      if(--(*ref_count) == 0) {
-        delete value;
-        delete ref_count;
-      }
+  ~smart_ptr()
+  {
+    if (--(*ref_count) == 0) {
+      delete value;
+      delete ref_count;
     }
+  }
 
-  private:
-    int* ref_count;
-    T* value;
-  };
+private:
+  int* ref_count;
+  T* value;
+};
 
 }
