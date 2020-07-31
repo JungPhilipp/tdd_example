ebfcbb11a95e376e99b73ff0ed6092911ad05edb
Philipp Jung <jung@volumegraphics.com>
2020-07-31 10:05:42 +0200

    Minor formatting

--- problem0/tests/reverse_char_ut.cpp
+++ problem0/tests/reverse_char_ut.cpp
@@ -12,26 +12,26 @@ TEST_CASE("Reverse String"){
   SUBCASE("Reverse non-empty even-length string"){
     char str[] = "testtest";
     problem0::reverse(str);
-        CHECK(std::string("tsettset") == str);
+    CHECK(std::string("tsettset") == str);
   }
   SUBCASE("Reverse one character string"){
     char str[] = "t";
     problem0::reverse(str);
-        CHECK(std::string("t") == str);
+    CHECK(std::string("t") == str);
   }
   SUBCASE("Reverse two character string"){
     char str[] = "ab";
     problem0::reverse(str);
-        CHECK(std::string("ba") == str);
+    CHECK(std::string("ba") == str);
   }
   SUBCASE("Reverse empty string"){
     char str[] = "";
     problem0::reverse(str);
-        CHECK(std::string("") == str);
+    CHECK(std::string("") == str);
   }
   SUBCASE("Reverse null string"){
     char * str = nullptr;
     problem0::reverse(str);
-        CHECK_EQ(str, nullptr);
+    CHECK_EQ(str, nullptr);
   }
 }
