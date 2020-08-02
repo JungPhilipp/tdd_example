7dbf20a030e99ff201be5c9b33266d40a9e18310
Philipp Jung <philipp.jung@mailbox.org>
2020-08-02 09:04:00 +0200

    WIP shared_ptr

--- shared_ptr/include/shared_ptr.h
+++ shared_ptr/include/shared_ptr.h
@@ -1,5 +1,23 @@
 #pragma once
+#include <cstdint>
 
-namespace tdd{
+namespace tdd {
+// TODO add custom deleter
+template<class T>
+class shared_ptr
+{
+public:
+  constexpr shared_ptr() noexcept;
+  constexpr shared_ptr(std::nullptr_t) noexcept;
+private:
+  T* value;
+  uint64_t ref_count;
+};
 
+// TODO add make_shared<>
+
+namespace test{
+// TODO add static test cases
+using integratl_type = int;
+}
 }
