6139970842a53f27a9b4df7826c18d17a98aa988
Philipp Jung <philipp.jung@mailbox.org>
2020-08-02 10:12:00 +0200

    Add templated solution for problem1

--- problem1/include/smallest_distance.h
+++ problem1/include/smallest_distance.h
@@ -1,5 +1,34 @@
 #pragma once
-
+#include "print.h"
+#include <algorithm>
+#include <array>
 namespace problem1 {
+
+/**
+ * Copy input range to allow for sorting without modifying original data
+ */
+template<class T, std::size_t S0, std::size_t S1>
+[[nodiscard]] consteval auto
+smallest_distance(std::array<T, S0> lhs, std::array<T, S1> rhs) noexcept
+  -> std::optional<std::pair<T, T>>
+{
+  if (size(lhs) == 0 || size(rhs) == 0)
+    return {};
+
+  std::ranges::sort(lhs);
+  std::ranges::sort(rhs);
+
+  auto a = begin(lhs);
+  auto b = begin(rhs);
+  auto distance = std::make_pair<std::size_t, std::size_t>(
+    std::numeric_limits<std::size_t>::max(), 0);
+  while (a != end(lhs) && b != end(rhs)) {
+    if (auto dist = *a - *b;
+        dist >= 0 && dist <= distance.first - distance.second)
+      distance = std::make_pair(*a, *b);
+    *a < *b ? a++ : b++;
+  }
+  return {distance};
+}
 
 }
--- problem1/tests/smallest_distance.cpp
+++ problem1/tests/smallest_distance.cpp
@@ -1,9 +1,31 @@
-#include "doctest.h"
 #include "smallest_distance.h"
-
+#include "doctest.h"
+#include <array>
 
 using namespace problem1;
 
-TEST_CASE("Reverse String"){
-
+TEST_CASE("Smallest Distance")
+{
+  SUBCASE("Small Positive Range")
+  {
+    auto constexpr lhs = std::array{ 1, 3, 15, 11, 2 };
+    auto constexpr rhs = std::array{ 23, 127, 235, 19, 8 };
+    auto constexpr pair = smallest_distance(lhs, rhs).value();
+    static_assert(pair.first == 11);
+    static_assert(pair.second == 8);
+  }
+  SUBCASE("Range including negative numbers")
+  {
+    auto constexpr lhs = std::array{ 1, 3, 15, 11, 2, -1 };
+    auto constexpr rhs = std::array{ 23, 127, 235, 19, 8, -2 };
+    auto constexpr pair = smallest_distance(lhs, rhs).value();
+    static_assert(pair.first == -1);
+    static_assert(pair.second == -2);
+  }
+  SUBCASE("Empty ranges")
+  {
+    auto constexpr lhs = std::array<int,0>{};
+    auto constexpr rhs = std::array<int,0>{};
+    static_assert(!smallest_distance(lhs, rhs));
+  }
 }
