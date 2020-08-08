7075f1bc45b10c7d91e8d14613526fee2bbf19d6
Philipp Jung <philipp.jung@mailbox.org>
2020-08-08 12:26:32 +0200

    Add solution for problem2

--- problem2/include/max_number.h
+++ problem2/include/max_number.h
@@ -1,8 +1,12 @@
 #pragma once
+#include <concepts>
+#include <cmath>
 
 namespace problem2{
 template <class T>
+requires std::integral<T> && std::signed_integral<T>
 [[nodiscard]] constexpr auto max_number(T lhs, T rhs) -> T {
-  return lhs;
+  auto k = !std::signbit(lhs - rhs);
+  return k * lhs + !k * rhs;
 }
 }
