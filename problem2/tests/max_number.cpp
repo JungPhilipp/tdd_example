8f8f76fbb030cb0142ecec407516d917f139a5fc
Philipp Jung <philipp.jung@mailbox.org>
2020-08-08 12:38:08 +0200

    Replace runtime tests with compile time tests

--- problem2/tests/max_number.cpp
+++ problem2/tests/max_number.cpp
@@ -5,15 +5,15 @@ using namespace problem2;
 
 TEST_CASE("Max Number") {
   SUBCASE("Positive Numbers"){
-    CHECK(max_number(10, 9) == 10);
-    CHECK(max_number(9, 10) == 10);
+    static_assert(max_number(10, 9) == 10);
+    static_assert(max_number(9, 10) == 10);
   }
   SUBCASE("Same number"){
-      CHECK(max_number(10, 10) == 10);
-      CHECK(max_number(0, 0) == 0);
+    static_assert(max_number(10, 10) == 10);
+    static_assert(max_number(0, 0) == 0);
   }
   SUBCASE("Negative number"){
-    CHECK(max_number(10, -10) == 10);
-    CHECK(max_number(-10, 0) == 0);
+    static_assert(max_number(10, -10) == 10);
+    static_assert(max_number(-10, 0) == 0);
   }
 }
